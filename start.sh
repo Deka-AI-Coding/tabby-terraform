dfb998a335f1d45a4acc36806a79ee0b66dc4289
SpeedCrash100 <SpeedCrash100@yandex.ru>
2024-02-24 07:37:06 +0300

    chore: update comment about VPNs

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# If using are VPNs you need to disable it or configure it first.
+# If you are using VPNs, make sure you can use the tabby properly. This line disables 
+# all active wireguard connections via NetworkManager.
 nmcli c | grep wireguard | awk '{ print $1 }' | xargs -n 1 nmcli c down || true
 
 terraform apply -auto-approve
