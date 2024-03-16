a7b6ab00217517bbcd4c91db6c234c80d798554f
SpeedCrash100 <SpeedCrash100@yandex.ru>
2024-03-16 11:22:41 +0300

    feat: use serve with --chat-device

--- main.tf
+++ main.tf
@@ -30,10 +30,7 @@ resource "docker_image" "tabby-worker-manager" {
   keep_locally = true
 }
 
-# Networks where all runners,workers will be
-resource "docker_network" "tabby_workers_net" {
-  name = "tabby_workers_net"
-}
+
 
 # Network connecting main https proxy with tabby
 resource "docker_network" "tabby_front_net" {
@@ -92,9 +89,16 @@ resource "docker_container" "certbot" {
 }
 
 resource "docker_container" "tabby-web" {
-  name    = "tabby-web"
-  image   = docker_image.tabby.image_id
-  command = ["serve", "--webserver"]
+  name  = "tabby-web"
+  image = docker_image.tabby.name
+  command = [
+    "serve",
+    "--webserver",
+    "--model", "${var.tabby_completion_model}",
+    "--device", "${var.tabby_completion_device}",
+    "--chat-model", "${var.tabby_chat_model}",
+    "--chat-device", "${var.tabby_chat_device}"
+  ]
   restart = "always"
   volumes {
     host_path      = pathexpand("~/.tabby")
@@ -102,35 +106,13 @@ resource "docker_container" "tabby-web" {
   }
   env = [
     "TABBY_WEBSERVER_JWT_TOKEN_SECRET=${var.tabby_jwt_token}",
-    "TABBY_DISABLE_USAGE_COLLECTION=1"
+    "TABBY_DISABLE_USAGE_COLLECTION=1",
+    "HSA_OVERRIDE_GFX_VERSION=10.3.0",
   ]
-
-  networks_advanced {
-    name = docker_network.tabby_workers_net.name
-  }
-
   networks_advanced {
     name = docker_network.tabby_front_net.name
   }
 
-}
-
-resource "docker_container" "tabby-chat-cpu" {
-  name       = "tabby-chat-cpu"
-  image      = docker_image.tabby.image_id
-  restart    = "always"
-  start      = true
-  depends_on = [docker_container.tabby-web]
-  command = [
-    "worker::chat",
-    "--url", "tabby-web:8080",
-    "--token", "${var.tabby_worker_token}",
-    "--model", "${var.tabby_chat_model}",
-    "--device", "cpu"
-  ]
-  env       = ["HSA_OVERRIDE_GFX_VERSION=10.3.0", "TABBY_DISABLE_USAGE_COLLECTION=1", "RUST_LOG=debug"]
-  group_add = ["video"]
-
   devices {
     host_path = "/dev/kfd"
   }
@@ -139,118 +121,6 @@ resource "docker_container" "tabby-chat-cpu" {
     host_path = "/dev/dri"
   }
 
-  volumes {
-    host_path      = pathexpand("~/.tabby")
-    container_path = "/data"
-  }
-
-  networks_advanced {
-    name = docker_network.tabby_workers_net.name
-  }
-}
-
-resource "docker_container" "tabby-chat-gpu" {
-  name       = "tabby-chat-gpu"
-  image      = docker_image.tabby.image_id
-  restart    = "always"
-  start      = false
-  must_run   = false
-  depends_on = [docker_container.tabby-web]
-  command = [
-    "worker::chat",
-    "--url", "tabby-web:8080",
-    "--token", "${var.tabby_worker_token}",
-    "--model", "${var.tabby_chat_model}",
-    "--device", "${var.tabby_gpu_device}"
-  ]
-  env       = ["HSA_OVERRIDE_GFX_VERSION=10.3.0", "TABBY_DISABLE_USAGE_COLLECTION=1", "RUST_LOG=debug"]
-  group_add = ["video"]
-
-  devices {
-    host_path = "/dev/kfd"
-  }
-
-  devices {
-    host_path = "/dev/dri"
-  }
-
-  volumes {
-    host_path      = pathexpand("~/.tabby")
-    container_path = "/data"
-  }
-
-  networks_advanced {
-    name = docker_network.tabby_workers_net.name
-  }
-}
-
-resource "docker_container" "tabby-completion-cpu" {
-  name       = "tabby-completion-cpu"
-  image      = docker_image.tabby.image_id
-  restart    = "always"
-  start      = false
-  must_run   = false
-  depends_on = [docker_container.tabby-web]
-  command = [
-    "worker::completion",
-    "--url", "tabby-web:8080",
-    "--token", "${var.tabby_worker_token}",
-    "--model", "${var.tabby_completion_model}",
-    "--device", "cpu"
-  ]
-  env       = ["HSA_OVERRIDE_GFX_VERSION=10.3.0", "TABBY_DISABLE_USAGE_COLLECTION=1", "RUST_LOG=debug"]
-  group_add = ["video"]
-
-  devices {
-    host_path = "/dev/kfd"
-  }
-
-  devices {
-    host_path = "/dev/dri"
-  }
-
-  volumes {
-    host_path      = pathexpand("~/.tabby")
-    container_path = "/data"
-  }
-
-  networks_advanced {
-    name = docker_network.tabby_workers_net.name
-  }
-}
-
-resource "docker_container" "tabby-completion-gpu" {
-  name       = "tabby-completion-gpu"
-  image      = docker_image.tabby.image_id
-  restart    = "always"
-  start      = true
-  depends_on = [docker_container.tabby-web]
-  command = [
-    "worker::completion",
-    "--url", "tabby-web:8080",
-    "--token", "${var.tabby_worker_token}",
-    "--model", "${var.tabby_completion_model}",
-    "--device", "${var.tabby_gpu_device}"
-  ]
-  env       = ["HSA_OVERRIDE_GFX_VERSION=10.3.0", "TABBY_DISABLE_USAGE_COLLECTION=1", "RUST_LOG=debug"]
-  group_add = ["video"]
-
-  devices {
-    host_path = "/dev/kfd"
-  }
-
-  devices {
-    host_path = "/dev/dri"
-  }
-
-  volumes {
-    host_path      = pathexpand("~/.tabby")
-    container_path = "/data"
-  }
-
-  networks_advanced {
-    name = docker_network.tabby_workers_net.name
-  }
 }
 
 resource "docker_container" "tabby-manager-api" {
