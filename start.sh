ea1b74dba936b57f9b2c9fcc745d6105b66e6485
SpeedCrash100 <SpeedCrash100@yandex.ru>
2024-09-02 15:22:51 +0300

    feat: disable autoconnect for connections

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # If you are using VPNs, make sure you can use the tabby properly. This line disables 
 # all active wireguard connections via NetworkManager.
 nmcli c | grep wireguard | awk '{ print $1 }' | xargs -n 1 nmcli c down || true
+nmcli c | grep wireguard | awk '{ print $1 }' | xargs -I {} -n 1 nmcli c mod {} connection.autoconnect false || true
 
 terraform apply -auto-approve
 
